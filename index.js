7abd2e7993a7389649d93f3dcb517cd9233062e4
NqobeeKay <166118462+NqobeeKay@users.noreply.github.com>
2024-05-26 02:55:18 +0200

    added a function to display the temperature of the city being searched for

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+function updateWeather(response) {
+  let temperatureElement = document.quarySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.quarySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "5d12c54o9b4080bae8eb90tf5232b23b";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+  axios.get(apiUrl).then(updateWeather);
+}
 function search(event) {
   event.preventDefault();
   let searchInput = document.quarySelector("#search-form-input");
-  let cityElement = document.quarySelector("#city");
-  cityElement.innerHTML = searchInput.value;
 }
 let searchFormElement = document.quarySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+searchCity("Paris");
